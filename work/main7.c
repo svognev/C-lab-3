5fc07bac6ab99e3de8554e30d456991afcccc157
efelzz <vatimofe@gmail.com>
2017-11-21 15:38:29 +0300

    Task 7 of 7 done

---
+++ work/main7.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define SIZE 1000
+#define N 256
+
+int main()
+{
+	char buf[SIZE] = { 0 };
+	int count[N] = { 0 };
+	int max = 0;
+	int i = 0;
+	printf("Enter some string:\n");
+	fgets(buf, SIZE, stdin);
+	for (int j = 0; j < N; j++)
+	{
+		if (buf[j] == '\n') {
+			buf[j] = '\0';
+			break;
+		}
+	}
+	while (buf[i] != '\0')
+	{
+		count[buf[i] - '0']++;
+		i++;
+	}
+
+	for (i = 0; i < N; i++)
+	{
+		if (count[i] > max)
+			max = count[i];
+	}
+
+	while (max > 0)
+	{
+		for (int k = 0; k < N; k++)
+		{
+			if (count[k] == max)
+				printf("%c-%d\n", k + '0', count[k]);
+		}
+		max--;
+	}
+
+	return 0;
+}
