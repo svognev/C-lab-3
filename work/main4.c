84a4de541d3451ea9d477df64688f4cd0811f48b
Andrey A Molkov <molkov_aa@mail.ru>
2017-12-09 20:05:16 +0300

    fix task4

--- work/main4.c
+++ work/main4.c
@@ -1,16 +1,16 @@
 #include "task4.h"
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
+ #include<stdio.h>
+ #include<stdlib.h>
+#include<string>
+ int main()
+ {
 	char buf[SIZE] = { ' ' };
 	int lenght = 0;
 	printf("Enter random set letters and digit, without gap : ");
-
-	fgets(buf, SIZE, stdin);
+	
+		fgets(buf, SIZE, stdin);
 	buf[strlen(buf) - 1] = '\0';
-
-
-	printf("\nSumma the all digit = %i\n", getSum(buf));
-	return 0 ;
-}
+	
+		
+		printf("\nSumma the all digit = %i\n", getSum(buf));
+	return 0;
--- work/task4.c
+++ work/task4.c
@@ -1,35 +1,35 @@
 #define IN 0
 #define OUT 1
-#include "task4.h"
-#include<stdlib.h>
+ #include "task4.h"
+ #include<stdlib.h>
+#include<string>
 int getSum(char buf[])
-{
+ {
 	int   i = 0, digit, count = 0, limit = LIMIT;
-	unsigned long result= 0, sum = 0;
-	 digit = OUT;
-	while ( i<strlen(buf))
-	{
-		if ((buf[i])>= '0' && (buf[i] <= '9')&& digit==OUT)// check digit
-		{
+	unsigned long result = 0, sum = 0;
+	digit = OUT;
+	while (i<strlen(buf))
+		 {
+		if ((buf[i]) >= '0' && (buf[i] <= '9') && digit == OUT)// check digit
+			 {
 			digit = IN;
 			result = 0;
 			result = result * 10 + (buf[i] - '0');
 			i++;
 			for (;(buf[i] >= '0' && (buf[i] <= '9') && digit == IN);)// the compilation digit
-			{
-				result = result*10+(buf[i] - '0');
+				 {
+				result = result * 10 + (buf[i] - '0');
 				
-				if (limit < count++)
-					digit = OUT;
-				//count++;
-				i++;
- 			}
+					if (limit < count++)
+					 digit = OUT;
+							//count++;
+					i++;
+				}
 			digit = OUT;
 			count = 0;
 			sum += result;// summa of all digit
-		}
+			}
 		i++;
-
-	}
+			}
 	return sum;
-}
+	}
