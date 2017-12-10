8cc53c1f0a224429fd98e6ca2bc98838f1fdf3d9
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-12-11 01:42:20 +0300

    Create task4.c

---
+++ work/task4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define N 50
+#define LEN_DIGIT 2
+
+int getSum(char str[])
+{
+	char *end;
+	int sum = 0;
+	char ch;
+	int inDigit = 0;
+	int i = 0;
+	while (str[i])
+	{
+		if (isdigit(str[i]))
+		{
+			inDigit = 0;
+
+			ch = str[i + LEN_DIGIT];
+			str[i + LEN_DIGIT] = '\0';
+			
+			inDigit = strtol(&str[i], &end, 10);
+			sum += inDigit;
+			
+			str[i + LEN_DIGIT] = ch;
+			i = i + (end - &str[i]);
+			
+		}
+		else if (!isdigit(str[i]))
+			i++;
+	}
+	return sum;
+}
