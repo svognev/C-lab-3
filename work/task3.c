0a81bfa15860951ad0c3d28ab9f8e38175021d8d
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-12-11 01:37:39 +0300

    Create task3.c

---
+++ work/task3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define N 100
+
+int getMaxWord(char buf[], char word[])
+{
+	int slovo, count = 0, len = 0;
+	int inWord = 0;
+	int i = 0, j = 0;
+	int maxLen = 0;
+
+	while (buf[i])
+	{
+		if (buf[i] != ' ' && inWord == 0)
+		{
+			inWord = 1;
+			len = 1;
+		}
+		else if ((buf[i] == ' ') && (inWord == 1))
+		{
+			if (len > maxLen)
+			{
+				maxLen = len;
+				int temp = len;
+				for (int k = 0; k < len; k++)
+				{
+					word[k] = buf[i - temp];
+					temp--;
+				}
+			}
+			len = j = 0;
+		}
+		else if (buf[i] != ' ' && inWord == 1)
+		{
+			len++;
+		}
+		i++;
+	}
+
+	return maxLen;
+}
