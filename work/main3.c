54cb5d6deb41943cc9d1006e205681d58ca1186b
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-12-11 01:36:43 +0300

    Create main3.c

---
+++ work/main3.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"task3.h"
+
+#define N 100
+
+int main()
+{
+	int len = 0;
+	char buf[N] = { 0 };
+	char word[N] = { 0 };
+
+	printf("Enter line \n");
+	fgets(buf, N, stdin);
+	len = strlen(buf);
+	buf[len - 1] = '\0 ';
+
+	printf("Max word: %s - %d  \n", &word, getMaxWord(buf, word));
+
+	return 0;
+}
