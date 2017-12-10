274c0f058f5d80d657fab1e87f3678fac508fb6f
Midory7 <33065952+Midory7@users.noreply.github.com>
2017-12-11 01:44:54 +0300

    Create main5.c

---
+++ work/main5.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "task5.h"
+
+#define N 7
+#define DIG 19
+
+int main()
+{
+	int buf[N] = {0};
+	int i = 0, j=0;
+	int p = 0; // count positive 
+	int n = 0; // count negative 
+	int ch=0;
+	
+	srand(time(0));
+	
+	for (i; i < N; i++)
+	{
+		ch = 0;
+		ch = rand() % DIG - (DIG / 2);
+		
+		if ((ch < 0) && (n < (N/2))) 
+		{
+			buf[i] = ch;
+			n++;
+		}
+
+		if ((ch >= 0) && (p < (N/2))) 
+		{
+			buf[i] = ch;
+			p++;
+		}
+		printf("%d \n",buf[i]);
+	}
+
+	printf("sum %d \n", getSumInt(buf, N));
+
+	return 0;
+}
