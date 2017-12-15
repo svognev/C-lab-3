c5ffc7969f602c34e65f61d122fa2f4bb26d9b10
AndreAMolkov <molkov_aa@mail.ru>
2017-12-15 13:13:14 +0300

    fix 5

--- work/task5.c
+++ work/task5.c
@@ -6,21 +6,32 @@
 
 int getSumInt(int arr[],int N)
 {
-	int summ = 0, i = 0,j=0;
-	while (arr[i++] > 0);//lookout the first negative number
-	while (arr[N--] < 0);//lookout the last positive number
-
-/*	while (j<i)//1part.  it's need for imaging solutions
+	int summ = 0, i = 0,j=0, max=0,min=0;
+	for (;i < N;i++)//lookout the first negative number
 	{
-		printf(" %i",summ);
-		j++;
-	}*/
-	i--;// the found number is not taken into account
-	while (i++ < N)
-			{
-				summ += arr[i];
-				//printf(" %i", arr[i]);//2 part. it's need for imaging solutions
+		if (arr[i] < 0) {
+			min = arr[i++];
+			break;
+		}
+	}
+	
+		for (;(N >= i );N--)//lookout the last positive number
+		{
+			if (arr[N] > 0) {
+				max = arr[N];
+				break;
 			}
-
+		}
+	
+	
+		printf("min= %i   ",min);
+	
+	
+		for (;i < N;i++)
+			{
+				summ = summ+arr[i];
+				printf(" %i", arr[i]);//2 part. it's need for imaging solutions
+			}
+	printf("    max= %i\n", max);
 	return summ;
 }
