cc98092c7ee06f42c132cf0cf6a5da6df97f7002
MartianovAV <Andrei.martianoff92@yandex.ru>
2018-01-17 16:50:44 +0300

    +lab3.4 test

---
+++ work/main4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#define SIZE 512
+
+
+long long int Conv(char buf_int[], int ind_start, int ind_end)
+{
+	long long int number = 0;
+	for()
+
+
+	return 0;
+}
+
+int getSum(char buf[]) //нахождение суммы
+{
+	int sum = 0; // Сумма элементов
+	int ind = 0; //Индекс элемента
+	long long int number = 0; //В данну юпеременную предполагается записывать числа полученные из массива
+
+
+
+
+	return sum;
+}
+
+
+
+int main()
+{
+	char buf[SIZE] = { 0 };	//Строка вводимая пользователем
+	printf("Enter a text string: ");
+	fgets(buf, SIZE, stdin);
+	buf[strlen(buf) - 1] = 0;
+	printf("sum = %d", getSum(buf));
+
+
+
+	_getch();
+}
