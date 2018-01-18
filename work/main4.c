1a8f293e334fe0d30f93fbc017bb1e3ba094ccf7
MartianovAV <Andrei.martianoff92@yandex.ru>
2018-01-18 11:06:53 +0300

    ...

--- work/main4.c
+++ work/main4.c
@@ -4,6 +4,23 @@
 #define SIZE 512
 
 
+//Вставка "_" по вереданному индексу, при этом производится сдвиг следующих элементов
+int shift(char str[], int ind)
+{
+	for (int i = strlen(str); ind < i; ind--)
+	{
+		str[i] = str[i - 1];
+	}
+	str[ind] = '_';
+	return 0;
+}
+
+/*
+int foo(char buf[], int size)
+{
+
+}*/
+
 //Преобразование строки в signed long long int
 signed long long int str_to_slli(char str[]) {
 	unsigned int strLen = 0;
@@ -67,6 +84,12 @@ int getSum(char buf[])
 int main()
 {
 	char buf[SIZE] = { 0 };	//Строка вводимая пользователем
+	//----------------------------------------------------------------------------------------------------------
+	int SIZE_NAMBER = 0;/*Если SIZE_NAMBER = 0 то при необходимости разбивка осуществляется с сохранением 
+	наибольшего количества разрядов первого числа, иначе SIZE_NAMBER задаёт мааксимальное количество разрядов*/
+	//----------------------------------------------------------------------------------------------------------
+
+
 	printf("Enter a text string: ");
 	fgets(buf, SIZE, stdin);
 	buf[strlen(buf) - 1] = 0;
